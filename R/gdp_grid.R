f6e3c59f1452d69cdde0e3ea13c718e8ec7bfa42
Nowosad <tupiszakaczki@gmail.com>
2018-01-30 11:26:41 -0500

    data docs added

---
+++ R/gdp_grid.R
@@ -0,0 +1,9 @@
+#' Gridded GDPs
+#'
+#' A dataset containing gridded GDPs (unit: PPP, Billion US$2005/yr) for years 1980-2100.
+#' Values from 1980-2010 were estimated by downscaling country-level data,
+#' while those in 2020-2100 are estimated by downscaling country-level data
+#' from SSP1, SSP2, and SSP3 scenarios.
+#'
+#' @source \url{https://github.com/Nowosad/global_population_and_gdp}
+"gdp_grid"
---
+++ R/pop_grid.R
@@ -0,0 +1,9 @@
+#' Gridded Populations
+#'
+#' A dataset containing gridded populations (unit: millions) for years 1980-2100.
+#' Values from 1980-2010 were estimated by downscaling country-level data,
+#' while those in 2020-2100 are estimated by downscaling country-level data
+#' from SSP1, SSP2, and SSP3 scenarios.
+#'
+#' @source \url{https://github.com/Nowosad/global_population_and_gdp}
+"pop_grid"
